d8eaa5e147e31efd4fc3a8adfbcb6ca62609bd9e
Sachin Kumar <sachinmandal726@gmail.com>
2024-11-04 22:21:30 +0530

    file setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 
 const app = express();
 
-const port =3000
+const port = process.env.PORT;
 
 app.get('/', (req, res) =>{
     res.send('Hello world')
@@ -19,7 +21,7 @@ app.get('/youtube', (req, res) =>{
     res.send('<h1> Welcome to Youtube')
 })
 
-app.listen(port, () =>{
+app.listen(process.env.PORT, () =>{
     console.log(`Example app listening on port ${port}`)
 })
 
